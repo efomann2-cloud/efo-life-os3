8dadf264946afa2593f0312e9b0100313ce5a23e
efomann2-cloud <efomann2@gmail.com>
2026-07-25 12:57:06 +0300

    Create progress_screen.dart

---
+++ lib/screens/progress/progress_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class ProgressScreen extends StatelessWidget {
+  const ProgressScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const Center(child: Text('Progress — Phase 3 ላይ እንገነባዋለን'));
+}
