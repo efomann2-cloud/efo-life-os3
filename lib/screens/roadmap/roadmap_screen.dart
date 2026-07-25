7b0a5ed1621eec86cf6e5f5e9539aaf5d6556779
efomann2-cloud <efomann2@gmail.com>
2026-07-25 12:59:40 +0300

    Create roadmap_screen.dart

---
+++ lib/screens/roadmap/roadmap_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class RoadmapScreen extends StatelessWidget {
+  const RoadmapScreen({super.key});
+  @override
+  Widget build(BuildContext context) => const Center(child: Text('Roadmap — Phase 5 ላይ እንገነባዋለን'));
+}
